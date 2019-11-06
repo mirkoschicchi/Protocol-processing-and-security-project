676f8908a3e05e2ba0a84fc116ebeb118468262f
Stephan Hauser <stephan@codefreeze.ch>
2019-11-06 17:05:16 +0200

    Add empty maven project

---
+++ protsim/src/main/java/fi/utu/protproc/group3/App.java
@@ -0,0 +1,13 @@
+package fi.utu.protproc.group3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
