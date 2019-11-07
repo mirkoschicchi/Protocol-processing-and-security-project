c32185e561d75007b2a5064dbb9dfc24c3ad7512
Stephan Hauser <stephan@codefreeze.ch>
2019-11-07 22:15:38 +0200

    Add NetworkAddress utility class

--- protsim/src/main/java/fi/utu/protproc/group3/simulator/Network.java
+++ protsim/src/main/java/fi/utu/protproc/group3/simulator/Network.java
@@ -1,5 +1,7 @@
 package fi.utu.protproc.group3.simulator;
 
+import fi.utu.protproc.group3.utils.NetworkAddress;
+
 public interface Network {
     static Network create() {
         throw new UnsupportedOperationException();
@@ -9,4 +11,9 @@ public interface Network {
      * Gets all devices attached to the network (simulated ARP scan)
      */
     Iterable<EthernetInterface> getDevices();
+
+    /**
+     * Gets the network address for this network.
+     */
+    NetworkAddress getNetworkAddress();
 }
