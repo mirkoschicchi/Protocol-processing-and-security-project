b65374bcc69f140c0239140ea9a6a4c56eed3616
Gabriele Orazi <gabriorazi07@gmail.com>
2019-11-08 00:18:50 +0200

    fix Update message, now uses NetworkAddress class rather then InetAddress

--- protsim/src/main/java/fi/utu/protproc/group3/protocols/bgp4/BGP4MessageUpdateImpl.java
+++ protsim/src/main/java/fi/utu/protproc/group3/protocols/bgp4/BGP4MessageUpdateImpl.java
@@ -1,18 +1,18 @@
 package fi.utu.protproc.group3.protocols.bgp4;
 
-import java.net.InetAddress;
+import fi.utu.protproc.group3.utils.NetworkAddress;
 import java.util.List;
 
 public interface BGP4MessageUpdateImpl extends BGP4MessageImpl {
     // TODO: (see below) Missing path attributes
-    static BGP4MessageUpdateImpl create(int withdrawnRoutesLength, List<InetAddress> withdrawnRoutes, int totalPathAttributeLength,
-                                        List<InetAddress> networkLayerReachabilityInformation) {
+    static BGP4MessageUpdateImpl create(int withdrawnRoutesLength, List<NetworkAddress> withdrawnRoutes, int totalPathAttributeLength,
+                                        List<NetworkAddress> networkLayerReachabilityInformation) {
         throw new UnsupportedOperationException();
     }
 
     int getWithdrawnRoutesLength();
-    List<InetAddress> getWithdrawnRoutes();
+    List<NetworkAddress> getWithdrawnRoutes();
     int getTotalPathAttributeLength();
     // TODO: Path attributes
-    List<InetAddress> getNetworkLayerReachabilityInformation();
+    List<NetworkAddress> getNetworkLayerReachabilityInformation();
 }
