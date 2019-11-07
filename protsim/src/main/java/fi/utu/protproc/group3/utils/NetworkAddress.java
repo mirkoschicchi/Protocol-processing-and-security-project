c01765c505427cccef04903b994d76088357a5ae
Stephan Hauser <stephan@codefreeze.ch>
2019-11-07 22:28:38 +0200

    Add support to parse and format CIDR notation network addresses

--- protsim/src/main/java/fi/utu/protproc/group3/utils/NetworkAddress.java
+++ protsim/src/main/java/fi/utu/protproc/group3/utils/NetworkAddress.java
@@ -1,6 +1,8 @@
 package fi.utu.protproc.group3.utils;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Objects;
 
 /**
  * Class to represent a CIDR network address in IPv4 or IPv6.
@@ -8,6 +10,23 @@ import java.net.InetAddress;
 public final class NetworkAddress {
     private final InetAddress address;
     private final int prefixLength;
+
+    /**
+     * Parses a network address in CIDR notation.
+     */
+    public static NetworkAddress parse(String cidr) throws UnknownHostException {
+        Objects.requireNonNull(cidr);
+
+        var lengthPos = cidr.indexOf('/');
+        if (lengthPos == -1) {
+            throw new IllegalArgumentException("Error in CIDR notation: Prefix length missing.");
+        }
+
+        var inetAddr = InetAddress.getByName(cidr.substring(0, lengthPos));
+        var prefixLength = Integer.parseInt(cidr.substring(lengthPos + 1));
+
+        return new NetworkAddress(inetAddr, prefixLength);
+    }
 
     public NetworkAddress(InetAddress address, int prefixLength) {
         this.address = address;
@@ -20,5 +39,10 @@ public final class NetworkAddress {
 
     public int getPrefixLength() {
         return prefixLength;
+    }
+
+    @Override
+    public String toString() {
+        return address.toString().substring(1) + "/" + prefixLength;
     }
 }
---
+++ protsim/src/test/java/fi/utu/protproc/group3/utils/NetworkAddressTest.java
@@ -0,0 +1,26 @@
+package fi.utu.protproc.group3.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.UnknownHostException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class NetworkAddressTest {
+    @Test
+    public void parseIPv4CidrNotation() throws UnknownHostException {
+        var netAddr = NetworkAddress.parse("127.0.0.0/8");
+
+        assertNotNull(netAddr);
+        assertEquals("127.0.0.0/8", netAddr.toString());
+    }
+
+    @Test
+    public void parseIPv6CidrNotation() throws UnknownHostException {
+        var netAddr = NetworkAddress.parse("fe80:2001::17:0:0/96");
+
+        assertNotNull(netAddr);
+        assertEquals("fe80:2001:0:0:0:17:0:0/96", netAddr.toString());
+    }
+}
