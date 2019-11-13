ba91d2c56c885b5ec9b0b540edd47d7f22954242
Stephan Hauser <stephan@codefreeze.ch>
2019-11-13 11:30:20 +0200

    Add unit test to parse TCP datagram

--- protsim/src/test/java/fi/utu/protproc/group3/protocols/TCPDatagramTest.java
+++ protsim/src/test/java/fi/utu/protproc/group3/protocols/TCPDatagramTest.java
@@ -1,5 +1,6 @@
 package fi.utu.protproc.group3.protocols;
 
+import fi.utu.protproc.group3.TestUtils;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -44,5 +45,26 @@ class TCPDatagramTest {
         assertEquals(original.getUrgentPointer(), reassembled.getUrgentPointer());
         assertArrayEquals(original.getOptionsAndPadding(), reassembled.getOptionsAndPadding());
         assertArrayEquals(original.getPayload(), reassembled.getPayload());
+    }
+
+    @Test
+    void parseTCPDatagram() {
+        var pdu = TestUtils.parseHexStream("00000000000000000000000086dd6005796e019d0640fddf5af267a400000000000000000a4ffddf5af267a400000000000000000a4f85761f90292e663bfd11106a80180200972e00000101080a1e678b161e678b16474554202f20485454502f312e310d0a486f73743a205b666464663a356166323a363761343a3a6134665d3a383038300d0a557365722d4167656e743a204d6f7a696c6c612f352e3020285831313b204c696e7578207838365f36343b2072763a37312e3029204765636b6f2f32303130303130312046697265666f782f37312e300d0a4163636570743a20746578742f68746d6c2c6170706c69636174696f6e2f7868746d6c2b786d6c2c6170706c69636174696f6e2f786d6c3b713d302e392c2a2f2a3b713d302e380d0a4163636570742d4c616e67756167653a20656e2d55532c656e3b713d302e382c64652d43483b713d302e352c64653b713d302e330d0a4163636570742d456e636f64696e673a20677a69702c206465666c6174650d0a444e543a20310d0a436f6e6e656374696f6e3a206b6565702d616c6976650d0a557067726164652d496e7365637572652d52657175657374733a20310d0a43616368652d436f6e74726f6c3a206d61782d6167653d300d0a0d0a");
+
+        var frame = EthernetFrame.parse(pdu);
+        var packet = IPv6Packet.parse(frame.getPayload());
+        var datagram = TCPDatagram.parse(packet.getPayload());
+
+        assertEquals((short) 34166, datagram.getSourcePort());
+        assertEquals((short) 8080, datagram.getDestinationPort());
+        assertEquals(0x292e663b, datagram.getSeqN());
+        assertEquals(0xfd11106a, datagram.getAckN());
+        assertEquals(TCPDatagram.PSH | TCPDatagram.ACK, datagram.getFlags());
+        assertEquals(512, datagram.getWindow());
+        assertEquals((short) 0x972e, datagram.getChecksum());
+        assertEquals(0, datagram.getUrgentPointer());
+        assertArrayEquals(TestUtils.parseHexStream("0101080a1e678b161e678b16"), datagram.getOptionsAndPadding());
+        assertNotNull(datagram.getPayload());
+        assertEquals(381, datagram.getPayload().length);
     }
 }
