7d5fb4d24d27ed2acc24113ef6e97a315bf7f31c
filippopiconese <filippopiconese@hotmail.it>
2019-11-28 20:51:55 +0200

    Added red stroke when a node is shutted down and restore it upon it is started again

--- protsim/src/main/java/fi/utu/protproc/group3/simulator/UserInterfaceManager.java
+++ protsim/src/main/java/fi/utu/protproc/group3/simulator/UserInterfaceManager.java
@@ -7,19 +7,22 @@ import org.graphstream.ui.view.ViewerListener;
 import org.graphstream.ui.view.ViewerPipe;
 import org.jetbrains.annotations.NotNull;
 
+import java.awt.*;
 import java.util.Map;
 
 public class UserInterfaceManager implements ViewerListener {
     private boolean loop = true;
     private Map<String, NetworkNode> nodes;
+    private MultiGraph graph;
 
     public UserInterfaceManager(@NotNull Viewer viewer, MultiGraph graph, Map<String, NetworkNode> nodes) {
         this.nodes = nodes;
+        this.graph = graph;
         viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.HIDE_ONLY);
 
         ViewerPipe fromViewer = viewer.newViewerPipe();
         fromViewer.addViewerListener(this);
-        fromViewer.addSink(graph);
+        fromViewer.addSink(this.graph);
 
         while(loop) {
             fromViewer.pump();
@@ -37,9 +40,11 @@ public class UserInterfaceManager implements ViewerListener {
             var node = nodes.get(s);
             if (node.nodeIsRunning()) {
                 node.shutdown();
+                graph.getNode(s).addAttribute("ui.style", "stroke-mode: plain; stroke-width: 3px; stroke-color: red;");
                 System.out.println("Node SHUTDOWN: " + s);
             } else {
                 node.start();
+                graph.getNode(s).addAttribute("ui.style", "stroke-mode: none;");
                 System.out.println("Node START: " + s);
             }
         }
