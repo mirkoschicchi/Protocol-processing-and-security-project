24175b26e010fd359d04ea52524fc3807cd7a182
Mirko Schicchi <mirko.schicchi@gmail.com>
2019-11-30 15:41:43 +0100

    Small changes

--- protsim/src/main/java/fi/utu/protproc/group3/finitestatemachine/BGPEventContext.java
+++ protsim/src/main/java/fi/utu/protproc/group3/finitestatemachine/BGPEventContext.java
@@ -1,5 +1,11 @@
 package fi.utu.protproc.group3.finitestatemachine;
 
-public class BGPEventContext {
+import fi.utu.protproc.group3.utils.IPAddress;
 
+public class BGPEventContext {
+    private final IPAddress peer;
+
+    public BGPEventContext(IPAddress peer) {
+        this.peer = peer;
+    }
 }
